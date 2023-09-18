b34000549b3235812e269351effc214a689b93b1
mansur <20260249+mansurGI@users.noreply.github.com>
2023-09-18 13:52:56 +0200

    Doctrine installed, Container for MariDB created

--- symfony/src/Controller/IndexController.php
+++ symfony/src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): JsonResponse
+    public function index(EntityManager $em): JsonResponse
     {
+        dd($em->getConnection()->executeQuery('select * from users;'));
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/IndexController.php',
