9ea768ef9f0090c6f699e8d6b9dafb24ce21fd4e
mansur <20260249+mansurGI@users.noreply.github.com>
2023-09-24 01:59:09 +0200

    docker compose rename contaienr, fix in setup script, added composer.phar

--- setup.bash
+++ setup.bash
@@ -4,8 +4,8 @@ docker compose up -d
 
 docker exec fpm php /app/composer.phar install --quiet --no-interaction 
 
-docker exec mariadb mysql -u root -proot --execute='create database app character set 'utf8mb4';'
-docker exec mariadb mysql -u root -proot --execute='create database test character set 'utf8mb4';'
+docker exec database mariadb -u root -proot --execute='create database app character set 'utf8mb4';'
+docker exec database mariadb -u root -proot --execute='create database test character set 'utf8mb4';'
 
 docker exec fpm php /app/bin/console doctrine:migrations:migrate --env=dev --no-interaction
 
