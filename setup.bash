23f2c2f70befc02350f6b84a96e62d65ebf6fc18
mansur <20260249+mansurGI@users.noreply.github.com>
2023-09-24 01:52:50 +0200

    docker compose in setup script

--- setup.bash
+++ setup.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker compose up -d
+
 docker exec fpm php /app/composer.phar install --quiet --no-interaction 
 
 docker exec mariadb mysql -u root -proot --execute='create database app character set 'utf8mb4';'
