4529b2e62eb4e39e9333adcbb8500fc15f2cdbfe
mansur <20260249+mansurGI@users.noreply.github.com>
2023-09-24 02:20:00 +0200

    added output for php test script, added start/stop project scripts

--- symfony/bin/test.php
+++ symfony/bin/test.php
@@ -1,6 +1,10 @@
 <?php
+$output = [];
 
-exec('php console doctrine:database:drop --force --env=test --no-interaction');
-exec('php console doctrine:migrations:migrate --env=test --no-interaction');
-exec('php console doctrine:fixtures:load --purge-with-truncate --env=test --no-interaction');
-exec('php phpunit');
+exec('php console doctrine:database:drop --force --env=test --no-interaction', $output);
+exec('php console doctrine:migrations:migrate --env=test --no-interaction', $output);
+exec('php console doctrine:fixtures:load --purge-with-truncate --env=test --no-interaction', $output);
+exec('php phpunit', $output);
+
+echo PHP_EOL . 'TEST.PHP OUTPUT' . PHP_EOL . '--------' . PHP_EOL . PHP_EOL;
+array_map(function ($value) { echo $value . PHP_EOL; }, $output);
