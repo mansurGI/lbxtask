673f213a7ad10e3e490230288646155faffbb158
mansur <20260249+mansurGI@users.noreply.github.com>
2023-09-24 02:29:46 +0200

    fixes in test.php script

--- symfony/bin/test.php
+++ symfony/bin/test.php
@@ -1,10 +1,10 @@
 <?php
 $output = [];
 
-exec('php console doctrine:database:drop --force --env=test --no-interaction', $output);
-exec('php console doctrine:migrations:migrate --env=test --no-interaction', $output);
-exec('php console doctrine:fixtures:load --purge-with-truncate --env=test --no-interaction', $output);
-exec('php phpunit', $output);
+exec('php /app/bin/console doctrine:database:drop --force --env=test --no-interaction', $output);
+exec('php /app/bin/console doctrine:migrations:migrate --env=test --no-interaction', $output);
+exec('php /app/bin/console doctrine:fixtures:load --purge-with-truncate --env=test --no-interaction', $output);
+exec('php /app/bin/phpunit', $output);
 
 echo PHP_EOL . 'TEST.PHP OUTPUT' . PHP_EOL . '--------' . PHP_EOL . PHP_EOL;
 array_map(function ($value) { echo $value . PHP_EOL; }, $output);
