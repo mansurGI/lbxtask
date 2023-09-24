6531c736a9bb1988c46b3ecbc9c4b83fb6758830
mansur <20260249+mansurGI@users.noreply.github.com>
2023-09-24 02:36:39 +0200

    added if not exists for mysql crate database

--- install.bash
+++ install.bash
@@ -9,8 +9,8 @@ echo 'compose installing'
 docker exec fpm php /app/composer.phar install --quiet --no-interaction 
 
 echo 'creating databases'
-docker exec database mariadb -u root -proot --execute='create database app character set 'utf8mb4';'
-docker exec database mariadb -u root -proot --execute='create database test character set 'utf8mb4';'
+docker exec database mariadb -u root -proot --execute='create database if not exists app character set 'utf8mb4';'
+docker exec database mariadb -u root -proot --execute='create database if not exists test character set 'utf8mb4';'
 
 echo 'migrating migrations to databases'
 docker exec fpm php /app/bin/console doctrine:migrations:migrate --env=dev --no-interaction
