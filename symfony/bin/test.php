26f3ba79c42e441a904713e6a9e685ad84025ba5
mansur <20260249+mansurGI@users.noreply.github.com>
2023-09-24 16:55:45 +0200

    test.php command for phpunit fix - config file loading

--- symfony/bin/test.php
+++ symfony/bin/test.php
@@ -3,7 +3,7 @@ $output = [];
 
 exec('php /app/bin/console doctrine:migrations:migrate --env=test --no-interaction', $output);
 exec('php /app/bin/console doctrine:fixtures:load --purge-with-truncate --env=test --no-interaction', $output);
-exec('php /app/bin/phpunit', $output);
+exec('php /app/bin/phpunit /app/tests -c /app/phpunit.xml.dist', $output);
 
 echo PHP_EOL . 'TEST.PHP OUTPUT' . PHP_EOL . '--------' . PHP_EOL . PHP_EOL;
 array_map(function ($value) { echo $value . PHP_EOL; }, $output);
