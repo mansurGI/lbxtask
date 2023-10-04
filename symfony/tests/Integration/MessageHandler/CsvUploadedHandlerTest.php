327f6453004259372a45fe59a0986afc7bd2ebd1
mansur <20260249+mansurGI@users.noreply.github.com>
2023-10-04 16:56:53 +0200

    added assertion for risky test, namespaces fix

--- symfony/tests/Integration/MessageHandler/CsvUploadedHandlerTest.php
+++ symfony/tests/Integration/MessageHandler/CsvUploadedHandlerTest.php
@@ -8,7 +8,6 @@ use App\Service\CsvFileManager;
 use App\Service\CsvImportService;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\Console\Exception\LogicException;
 
 class CsvUploadedHandlerTest extends KernelTestCase
 {
@@ -29,6 +28,7 @@ class CsvUploadedHandlerTest extends KernelTestCase
         $handler = new CsvUploadedHandler($csvFileManagerMock, $logger, $csvImportServiceMock);
 
         $handler($message);
+        $this->assertTrue(true);
     }
 
     /**
@@ -44,12 +44,12 @@ class CsvUploadedHandlerTest extends KernelTestCase
 
         $csvImportServiceMock = $this->createMock(CsvImportService::class);
         $csvImportServiceMock->method('process')
-            ->willThrowException(new LogicException('Error on csv importing'));
+            ->willThrowException(new \LogicException('Error on csv importing'));
 
         $message = new CsvUploaded('some-file');
         $handler = new CsvUploadedHandler($csvFileManagerMock, $logger, $csvImportServiceMock);
 
-        $this->expectException(LogicException::class);
+        $this->expectException(\LogicException::class);
         $handler($message);
     }
 
