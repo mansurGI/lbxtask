db662551979e08455be32dc8dead9d35eef6002e
mansur <20260249+mansurGI@users.noreply.github.com>
2023-10-18 16:55:07 +0200

    composer moved into container

--- install.bash
+++ install.bash
@@ -12,7 +12,7 @@ echo 'docker up'
 docker compose up -d
 
 echo 'composer install'
-docker exec fpm php /app/composer.phar install --quiet --no-interaction 
+docker exec fpm composer install --quiet --no-interaction
 
 echo 'creating databases'
 docker exec database mariadb -u root -proot --execute='create database if not exists app character set 'utf8mb4';'
