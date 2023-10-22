f0c3e48ff50a2a8aef827126abe34b5ecd268a61
mansur <20260249+mansurGI@users.noreply.github.com>
2023-10-23 01:14:28 +0200

    removed useless references in fixtures

--- symfony/src/DataFixtures/EmployeeFixtures.php
+++ symfony/src/DataFixtures/EmployeeFixtures.php
@@ -11,7 +11,6 @@ class EmployeeFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $employee = new Employee();
-
         $employee->setAge(35.87);
         $employee->setBirthDate(\DateTime::createFromFormat('m/d/Y', '9/21/1982'));
         $employee->setBirthTime(\DateTime::createFromFormat('h:i:s A', '01:53:13 AM'));
@@ -32,35 +31,30 @@ class EmployeeFixtures extends Fixture
         $employee->setEid(198429);
         $employee->setUsername('sibumgarner');
         $employee->setZipcode(14724);
-
-        $this->addReference('employee-1', $employee);
         $manager->persist($employee);
 
-        $employee2 = new Employee();
-
-        $employee2->setAge(50.26);
-        $employee2->setBirthDate(\DateTime::createFromFormat('m/d/Y', '5/8/1976'));
-        $employee2->setBirthTime(\DateTime::createFromFormat('h:i:s A', '06:03:23 AM'));
-        $employee2->setCity('Glenside');
-        $employee2->setCounty('Montgomery');
-        $employee2->setEmail('juliette.rojo@yahoo.co.uk');
-        $employee2->setFirstname('Juliette');
-        $employee2->setGender('M');
-        $employee2->setJoinDate(\DateTime::createFromFormat('m/d/Y', '6/4/2011'));
-        $employee2->setLastname('Rojo');
-        $employee2->setMiddleInitial('I');
-        $employee2->setPrefix('Mrs.');
-        $employee2->setPhone('212-376-9125');
-        $employee2->setPlace('Glenside');
-        $employee2->setRegion('Northeast');
-        $employee2->setStatus(null);
-        $employee2->setTenure(6.15);
-        $employee2->setEid(178566);
-        $employee2->setUsername('jmrojo');
-        $employee2->setZipcode(19038);
-
-        $this->addReference('employee-2', $employee2);
-        $manager->persist($employee2);
+        $employee = new Employee();
+        $employee->setAge(50.26);
+        $employee->setBirthDate(\DateTime::createFromFormat('m/d/Y', '5/8/1976'));
+        $employee->setBirthTime(\DateTime::createFromFormat('h:i:s A', '06:03:23 AM'));
+        $employee->setCity('Glenside');
+        $employee->setCounty('Montgomery');
+        $employee->setEmail('juliette.rojo@yahoo.co.uk');
+        $employee->setFirstname('Juliette');
+        $employee->setGender('M');
+        $employee->setJoinDate(\DateTime::createFromFormat('m/d/Y', '6/4/2011'));
+        $employee->setLastname('Rojo');
+        $employee->setMiddleInitial('I');
+        $employee->setPrefix('Mrs.');
+        $employee->setPhone('212-376-9125');
+        $employee->setPlace('Glenside');
+        $employee->setRegion('Northeast');
+        $employee->setStatus(null);
+        $employee->setTenure(6.15);
+        $employee->setEid(178566);
+        $employee->setUsername('jmrojo');
+        $employee->setZipcode(19038);
+        $manager->persist($employee);
 
         $manager->flush();
     }
