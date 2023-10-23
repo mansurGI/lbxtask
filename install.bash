918124d3cabbefac1a8860a1a469d9f7fca40362
mansur <20260249+mansurGI@users.noreply.github.com>
2023-10-23 02:18:02 +0200

    password changes in install script

--- install.bash
+++ install.bash
@@ -15,8 +15,8 @@ echo 'composer install'
 docker exec fpm composer install --quiet --no-interaction
 
 echo 'creating databases'
-docker exec database mariadb -u root -proot --execute="create database if not exists app character set 'utf8mb4';"
-docker exec database mariadb -u root -proot --execute="create database if not exists test character set 'utf8mb4';"
+docker exec database mariadb -u root -psTlInaYSVdVnmv9uYI2gfj --execute="create database if not exists app character set 'utf8mb4';"
+docker exec database mariadb -u root -psTlInaYSVdVnmv9uYI2gfj --execute="create database if not exists test character set 'utf8mb4';"
 
 echo 'migrating migrations to databases'
 docker exec fpm php /app/bin/console doctrine:migrations:migrate --env=dev --no-interaction
